a9108fa89bdd6ed4cd429802a6a864ecc218b23f
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-11-17 09:05:15 +0000

    Add script to remove "import foo as foo" lines.

---
+++ no_as.py
@@ -0,0 +1,19 @@
+# stdlib
+import pathlib
+import re
+
+import isort  # type: ignore
+from isort import Config
+
+isort_config = Config(settings_file=str(".isort.cfg"))
+
+
+for filename in pathlib.Path("dulwich-stubs").rglob("*.pyi"):
+	# print(filename)
+	file_contents = filename.read_text()
+	for entry in re.findall(r"([A-Za-z0-9_.]+) as ([A-Za-z0-9_.]+)", file_contents):
+		if entry[0] == entry[1]:
+			file_contents = file_contents.replace(f"{entry[0]} as {entry[1]}", entry[0])
+
+	file_contents = isort.code(file_contents, config=isort_config)
+	filename.write_text(file_contents)
